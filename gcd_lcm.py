ca458eb4b6645c03ae2d03ab730606e588b0409e
joos <thisis.joos@gmail.com>
2021-08-21 13:12:34 +0000

    LCM 구하는 함수 추가

--- gcd_lcm.py
+++ gcd_lcm.py
@@ -15,3 +15,9 @@ def gcd_recur(a,b):
 
 print("60과 28의 최대공약수 - 반복 : " , gcd_iter(60,28))
 print("60과 28의 최대공약수 - 순환 : " , gcd_recur(60,28))
+
+def lcm(a, b):
+  return int(a * b / gcd_recur(a,b));
+
+
+print("6과 8의 최소공배수 : ", lcm(6,8))
