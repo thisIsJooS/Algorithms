5980139f82a7d6c8672c648a5394ce14bb508974
joos <thisis.joos@gmail.com>
2021-08-28 03:25:00 +0000

    string_matching.py 최초 업로드

---
+++ string_matching.py
@@ -0,0 +1,17 @@
+# 문자열 매칭 - 길이가 n인 문자열 T와 길이가 m인 패턴 문자열 P에서, T에서 가장 먼저 나타나는 P의 위치
+
+def string_matching_brute(T, P):
+  n=len(T)
+  m=len(P)
+  for i in range(n-m+1):
+    j=0
+    while j<m and P[j]==T[i+j]:
+      j=j+1
+    if j==m:
+      return i
+  return -1
+  
+T="dmikijkladajskladjalsjsakdsklmikidskjads"
+P="miki"
+
+print(string_matching_brute(T,P))
