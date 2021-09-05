ad7d470a01bc5f8960642b56596680b6e86ca625
joos <thisis.joos@gmail.com>
2021-09-05 00:35:03 +0000

    permutation.py 파일 업로드

---
+++ ch09/permutation.py
@@ -0,0 +1,34 @@
+# 백트래킹을 이용한 순열 생성
+
+def all_permutations(data):
+    selected = [False] * len(data)  # 필요한 자료 초기화
+    DFS_permutation(data, [], 0, selected)  # 순환 호출 시작
+
+
+def DFS_permutation(data, sol, level, selected):
+    if level == len(data):  # 함수가 끝나는 조건 (탐색이 끝나는 조건)
+        print(sol)
+        return
+
+    for i in range(len(data)):
+        if not selected[i]:  # 아직 사용되지 않았어야 가능한 부분해
+            sol.append(data[i])
+            selected[i] = True
+            DFS_permutation(data, sol, level+1, selected)  # 자식 노드 탐색
+            sol.pop()  # 복구 : 부분해에서 꺼냄 / 여기까지 왔다는 건 탐색에 실패했다는 뜻이므로 복구를 하는 것
+            selected[i] = False  # 복구 : 이 원소는 사용하지 않음 
+
+
+all_permutations(['A', 'B', 'C'])
+
+
+####################
+#결과
+#
+# ['A', 'B', 'C']
+# ['A', 'C', 'B']
+# ['B', 'A', 'C']
+# ['B', 'C', 'A']
+# ['C', 'A', 'B']
+# ['C', 'B', 'A']
+####################
