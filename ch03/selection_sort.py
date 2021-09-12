e2819d5c6813281a9b4e092758112ef8227eb31f
joos <thisis.joos@gmail.com>
2021-09-12 10:14:40 +0000

    selection_sort.py 코드 공백 수정

--- ch03/selection_sort.py
+++ ch03/selection_sort.py
@@ -1,21 +1,21 @@
 # Selection Sort Algorithm - O(N^2)
 
 def selection_sort(A):
-  n=len(A)
+  n = len(A)
   for i in range(n-1):
-    least=i
-    for j in range(i+1,n):
-      if(A[j]<A[least]):
-        least=j
-    A[i],A[least]=A[least],A[i]
-    printStep(A,i+1)
+    least = i
+    for j in range(i + 1,n):
+      if(A[j] < A[least]):
+        least = j
+    A[i], A[least] = A[least], A[i]
+    printStep(A, i+1)
     
 def printStep(arr, val):
   print("   Step%2d =  " %val, end="")
   print(arr)
 
 # 알고리즘 테스트
-data=[5,3,8,4,9,1,6,2,7]
+data=[5, 3, 8, 4, 9, 1, 6, 2, 7]
 print(" Original : ", data)
 selection_sort(data)
 print("Selection : ", data)
