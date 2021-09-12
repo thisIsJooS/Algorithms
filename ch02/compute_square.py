6e202a1266b1cba49fca3781508a4782e7d6d689
joos <thisis.joos@gmail.com>
2021-09-12 10:09:28 +0000

    compute_square.py 알고리즘 테스트 코드 추가

--- ch02/compute_square.py
+++ ch02/compute_square.py
@@ -4,7 +4,7 @@ import time
 
 # O(1)
 def compute_square_A(n):
-  return n*n
+  return n * n
 
 # O(n)
 def compute_square_B(n):
@@ -21,3 +21,24 @@ def compute_square_C(n):
       sum = sum + 1
   return sum
 
+
+# 알고리즘 테스트
+t1 = time.time()
+compute_square_A(8000)
+t2 = time.time()
+compute_square_B(8000)
+t3 = time.time()
+compute_square_C(8000)
+t4 = time.time()
+
+print("compute_square_A(8000) 걸린 시간 : %.8f" % (t2-t1))
+print("compute_square_B(8000) 걸린 시간 : %.8f" % (t3-t2))
+print("compute_square_C(8000) 걸린 시간 : %.8f" % (t4-t3))
+
+########################################################
+# 결과
+#
+# compute_square_A(8000) 걸린 시간 : 0.00000238
+# compute_square_B(8000) 걸린 시간 : 0.00282288
+# compute_square_C(8000) 걸린 시간 : 26.80734563
+########################################################
