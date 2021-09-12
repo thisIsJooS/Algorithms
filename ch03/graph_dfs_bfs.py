88155b58664c92e49a6274c0c702447e1afeefd6
joos <thisis.joos@gmail.com>
2021-09-12 10:12:47 +0000

    graph_dfs_bfs.py 코드 순서  수정

--- ch03/graph_dfs_bfs.py
+++ ch03/graph_dfs_bfs.py
@@ -1,49 +1,55 @@
 import queue
 
 graph={
-  "A":{"B","C"},
-  "B":{"A","D"},
-  "C":{"A","D","E"},
-  "D":{"B","C","F"},
-  "E":{"C","G","H"},
-  "F":{"D"},
-  "G":{"E","H"},
-  "H":{"E","G"}
+  "A" : {"B", "C"},
+  "B" : {"A", "D"},
+  "C" : {"A", "D", "E"},
+  "D" : {"B", "C", "F"},
+  "E" : {"C", "G", "H"},
+  "F" : {"D"},
+  "G" : {"E", "H"},
+  "H" : {"E", "G"}
 }
 
-# 깊이 우선 탐색
+# Depth First Search
 def dfs(graph, start, visited):
   if start not in visited:
     visited.add(start)
     print(start, end=' ')
-    nbr=graph[start]-visited
+    nbr = graph[start] - visited
+    
     for v in nbr:
       dfs(graph, v, visited)
       
-print('DFS : ', end='')
-dfs(graph, "A", set())
-print()
 
-# 너비 우선 탐색
+# Breadth First Search
 def bfs(graph, start):
-  visited={start}
-  que=queue.Queue()
+  visited = { start }
+  que = queue.Queue()
   que.put(start)
+  
   while not que.empty():
-    v=que.get()
-    print(v, end=" ")
-    nbr=graph[v]-visited
+    v = que.get()
+    print(v, end = " ")
+    nbr = graph[v] - visited
     for u in nbr:
       visited.add(u)
       que.put(u)
 
+
+# 알고리즘 테스트
+
+print('DFS : ', end='')
+dfs(graph, "A", set())
+print()
+
 print('BFS : ',end='')
 bfs(graph, "A")
-print()
 
 
 ############################################################################
 # 결과 - 딕셔너리는 순서가 없으므로 실핼할 때 마다 결과가 달라진다. 그렇다고 틀린 건 아니다.
+#
 # DFS : A C E G H D F B
 # BFS : A C B E D G H F
 ############################################################################
