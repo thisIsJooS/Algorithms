aa6118d70f9728b910e0d953afdfe21609b20c76
joos <thisis.joos@gmail.com>
2021-10-01 04:00:15 +0000

    dfs 기반 topological sort 함수 추가

--- ch04/topological_sort.py
+++ ch04/topological_sort.py
@@ -7,29 +7,81 @@ directedGraph = {
   'F' : {}
 }
 
-def topological_sort(graph):
-  degrees = {}
+def topologicalSort_DAC(graph):
+  inDeg = {}
 
   for v in graph:
-    degrees[v] = 0
+    inDeg[v] = 0
   
   for v in graph:
     for u in graph[v]:
-      degrees[u] += 1
+      inDeg[u] += 1
   
   vlist = []
 
   for v in graph:
-    if degrees[v] == 0 :
+    if inDeg[v] == 0 :
       vlist.append(v)
   
   while vlist:
     v = vlist.pop()
-    print(v)
+    print(v, end = ' ')
+    
     for u in graph[v]:
-      degrees[u] -= 1
-      if degrees[u] == 0:
+      inDeg[u] -= 1
+      if inDeg[u] == 0:
         vlist.append(u)
 
-topological_sort(directedGraph)
-print() 
+topologicalSort_DAC(directedGraph)
+print()
+
+###############
+# 결과
+#
+# B E A C D F
+################
+
+
+
+
+def topologicalSort_DFS(graph):
+  inDeg = {}
+  starts = []
+  
+  for v in graph:
+    inDeg[v] = 0
+    
+  for v in graph:
+    for u in graph[v]:
+      inDeg[u] += 1
+  
+  for v in graph:
+    if inDeg[v] == 0:
+      starts.append(v)
+      
+  for start in starts:
+    dfs(graph, start, [], inDeg)
+
+
+def dfs(graph, v, visited, inDeg):
+  if v not in visited:
+    visited.append(v)
+    print(v, end = ' ')
+    
+    for u in graph[v]:
+      inDeg[u] -= 1
+    
+    for u in graph[v]:
+      if inDeg[u] == 0:
+        dfs(graph, u, visited, inDeg)
+        
+topologicalSort_DFS(directedGraph)
+print()
+
+
+
+###############
+# 결과
+#
+# A C B E D F
+################
