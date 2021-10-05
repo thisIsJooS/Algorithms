2326e3e16882141286dbbcbdd038ddddb7108615
joos <thisis.joos@gmail.com>
2021-10-05 11:09:01 +0000

    string_matching() 함수명 변경

--- ch06/string_matching.py
+++ ch06/string_matching.py
@@ -1,6 +1,6 @@
 # 문자열 매칭 - 길이가 n인 문자열 T와 길이가 m인 패턴 문자열 P에서, T에서 가장 먼저 나타나는 P의 위치
 
-def string_matching_bf(T, P):
+def string_matching(T, P):
   n=len(T)
   m=len(P)
   for i in range(n-m+1):
@@ -14,4 +14,4 @@ def string_matching_bf(T, P):
 T="dmikijkladajskladjalsjsakdsklmikidskjads"
 P="miki"
 
-print(string_matching_bf(T,P))
+print(string_matching(T,P))
