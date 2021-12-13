6430576dbbca81c54a11ff0b78d6fe0421fed541
joos <thisis.joos@gmail.com>
2021-12-13 10:59:39 +0000

    upload files

---
+++ etc/rotate_a_matrix_by_90_degree.py
@@ -0,0 +1,34 @@
+def rotate_a_matrix_by_90_degree(a):    # 시계방향 회전 
+    n, m = len(a), len(a[0])    # 행, 열
+    
+    result = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            result[j][n-1-i] = a[i][j]
+            
+    return result
+
+
+def rotate_a_matrix_by_90_degree_reverse(a):    # 반시계방향 회전 
+    n, m = len(a), len(a[0])    # 행, 열
+    
+    result = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            result[n-1-j][i] = a[i][j]
+            
+    return result
+
+
+arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for a in rotate_a_matrix_by_90_degree(arr):
+    print(a)
+    
+print("=========")
+for a in rotate_a_matrix_by_90_degree_reverse(arr):
+    print(a)
